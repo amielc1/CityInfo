7dc373c401b2506ac58ed1029c91521c79904402
Amiel Cohen <amielc1@gmail.com>
2021-01-18 21:46:04 +0200

    Add Json configuration to container

--- CityInfo.API/Services/LocalMailService.cs
+++ CityInfo.API/Services/LocalMailService.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,14 +10,19 @@ namespace CityInfo.API.Services
     public class LocalMailService : IMailService
     {
         private readonly ILogger<LocalMailService> _logger;
+        private readonly IConfiguration _configuration;
 
-        public LocalMailService(ILogger<LocalMailService> logger)
+        public LocalMailService(ILogger<LocalMailService> logger, IConfiguration configuration)
         {
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
         }
         public void Send(string subject, string message)
         {
-            _logger.LogInformation($"Send LocalMailService Subject : {subject}, Message : {message} ");
+            _logger.LogInformation("Send LocalMailService");
+            _logger.LogInformation($"From : {_configuration["mailSettings:mailFromAddress"]} , To {_configuration["mailSettings:mailToAddress"]}");
+            _logger.LogInformation($"Subject : {subject}");
+            _logger.LogInformation($"Message : {message}");
         }
     }
 }
